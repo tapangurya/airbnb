dc6dc3ac76c4672fdd2476093c61d24414ac279b
Tapan Gurya <tapangurya1234@gmail.com>
2026-01-12 23:29:42 +0530

    secure...SA

--- routes/superAuthRouter.js
+++ routes/superAuthRouter.js
@@ -6,9 +6,9 @@ superAuthRouter.get("/index",  superAuthController.getIndex);
 // Login
 superAuthRouter.get("/super-login", superAuthController.getLogin);
 superAuthRouter.post("/super-login", superAuthController.postLogin);
-// Register
-superAuthRouter.get("/super-signup", superAuthController.getRegister);
-superAuthRouter.post("/super-signup", superAuthController.postRegister);
+//!---------------------- Register ------------------------------
+// superAuthRouter.get("/super-signup", superAuthController.getRegister);
+// superAuthRouter.post("/super-signup", superAuthController.postRegister);
 
 // Logout
 superAuthRouter.post("/logout",superAuthController.postLogout);
